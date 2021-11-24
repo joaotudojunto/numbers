7ebcbcd58e762c2effb3513d7b2f46fee93f38c6
Joao Duarte <joaoduart@live.com.pt>
2021-11-24 15:33:44 +0000

    Passo 4

---
+++ src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -0,0 +1,54 @@
+package com.aor.numbers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListAggregatorTest {
+
+    public List<Integer> helperMethod(){
+        return Arrays.asList(1,2,4,2,5);
+    }
+
+    @Test
+    public void sum() {
+        List<Integer> list = helperMethod();
+
+        ListAggregator aggregator = new ListAggregator();
+        int sum = aggregator.sum(list);
+
+        Assertions.assertEquals(14, sum);
+    }
+
+    @Test
+    public void max() {
+        List<Integer> list = helperMethod();
+
+        ListAggregator aggregator = new ListAggregator();
+        int max = aggregator.max(list);
+
+        Assertions.assertEquals(5, max);
+    }
+
+    @Test
+    public void min() {
+        List<Integer> list = helperMethod();
+
+        ListAggregator aggregator = new ListAggregator();
+        int min = aggregator.min(list);
+
+        Assertions.assertEquals(1, min);
+    }
+
+    @Test
+    public void distinct() {
+        List<Integer> list = helperMethod();
+
+        ListAggregator aggregator = new ListAggregator();
+        int distinct = aggregator.distinct(list);
+
+        Assertions.assertEquals(4, distinct);
+    }
+}
