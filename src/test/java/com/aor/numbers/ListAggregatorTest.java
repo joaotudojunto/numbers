a5851f03232cea05a1be80c8c95041ccec542cff
Joao Duarte <joaoduart@live.com.pt>
2021-11-25 16:04:13 +0000

    Passo 6

--- src/test/java/com/aor/numbers/ListAggregatorTest.java
+++ src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -9,10 +9,10 @@ import java.util.List;
 public class ListAggregatorTest {
 
     public List<Integer> helperMethod(){
-        return Arrays.asList(-1,-4, -5);
+        return Arrays.asList(1,2,4,2);
         //return Arrays.asList(1,2,4,2,5);
     }
-    @Test
+
     public void max_bug_7263() {
         List<Integer> list = helperMethod();
 
@@ -24,13 +24,24 @@ public class ListAggregatorTest {
     }
 
     @Test
+    public void distinct_bug_8726() {
+        List<Integer> list = helperMethod();
+        ListAggregator aggregator = new ListAggregator();
+        int distinct = aggregator.distinct(list);
+        if(distinct != 3){
+            System.out.println("Bug Report #8726\nLists created with values 1, 2, 4, 2 and result should be 3, got " + distinct + " " + "instead");
+        }
+
+    }
+
+    @Test
     public void sum() {
         List<Integer> list = helperMethod();
 
         ListAggregator aggregator = new ListAggregator();
         int sum = aggregator.sum(list);
 
-        Assertions.assertEquals(-10, sum);
+        Assertions.assertEquals(9, sum);
 
     }
 
@@ -41,7 +52,7 @@ public class ListAggregatorTest {
         ListAggregator aggregator = new ListAggregator();
         int max = aggregator.max(list);
 
-        Assertions.assertEquals(-1, max);
+        Assertions.assertEquals(4, max);
     }
 
     @Test
@@ -51,7 +62,7 @@ public class ListAggregatorTest {
         ListAggregator aggregator = new ListAggregator();
         int min = aggregator.min(list);
 
-        Assertions.assertEquals(-5, min);
+        Assertions.assertEquals(1, min);
     }
 
     @Test
